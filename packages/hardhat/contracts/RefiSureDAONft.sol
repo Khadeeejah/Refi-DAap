dfde43a48326810928dfc2f78a4bbd4677ede9b0
chinwizu <chinwianthony@gmail.com>
2023-05-23 06:57:16 +0100

    nft

---
+++ packages/hardhat/contracts/RefiSureDAONft.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RefiSureDAONft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://QmTdFT4rN95DhQNDstJkZLjicvDjCpYb8JwGKRE3RwkFoy";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("RefiSureDAONft", "RSDNFT") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
